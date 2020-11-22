f3c398f9ed3970fb8afabc364f76a54af5e322aa
Sven Notermans <nootje27@outlook.com>
2020-11-22 15:22:33 +0100

    added mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const mongoose = require("mongoose")
+const {Schema} = mongoose
+require('dotenv').config()
 
-app.get('/', function (req, res) {
-   res.send('YYOOOOOOOOOOOOOOOOOOOOOOOooo');
+//mongodb+srv://netflix-api-dev:jwoc29q2BwUzvyw3@cluster0.jizn1.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.jizn1.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`, {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true});
+
+const User = mongoose.model('Users', new Schema(
+    {
+        name: String,
+        email:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    },
+));
+
+app.use(express.json());
+
+// using the get method
+// LOGIC for the Get Request
+// I'm trying to get data
+app.get('/', (req, res) => {
+    res.send("Hellow World")
+  })
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    newUser.save((err, user)=> {
+        console.log(err)
+        if(err){
+            console.log(err)
+            res.send(400, {
+                status: err
+            })
+        }else{
+            console.log("all is good")
+            console.log(user);
+            res.send('registered');
+        }
+    })
+})
+
+app.post('/login', (req, res) => {
+    const password = req.body.password;
+    const email = req.body.email;
+    User.findOne({email: email, password: password}, (err, user)=>{
+     console.log(user)
+    if(user){ 
+    res.send({
+        status: "valid"
+    })
+    }else{
+        res.send(404, {
+            status: "Not Found"
+        })
+    }
+})
 })
 
 app.listen(port, () => {
